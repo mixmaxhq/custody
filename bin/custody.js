af18fc23e47ec37af1a82f1f3ca624d6cb1e6f9c
Jeff Wear <jeffreywear@gmail.com>
2018-05-08 23:21:47 -0700

    Package as binary
    Fixes https://github.com/mixmaxhq/custody/issues/9.

---
+++ bin/custody.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+const custody = require('..');
+
+const argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .options({
+    port: {
+      describe: 'The port on which supervisor is running. 9001 is the default for Mixmax engineers.',
+      type: 'number',
+      default: 9001
+    }
+  })
+  .example('$0')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+custody(argv);
