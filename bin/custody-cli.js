974dc0c863debb9465873f98ddc4b516c401efbc
Jeff Wear <jeffreywear@gmail.com>
2018-05-16 01:30:29 -0700

    Optionally show desktop notifications when processes’ states change
    Clicking the "Show" button on the notification will activate the Terminal
    and open custody to the corresponding service's logs.
    
    This is optional and must be enabled by passing the `—notifications` flag to
    `custody-cli` in case it’s annoying in its current form. We should collect UX
    feedback and refine the notification preferences, debouncing, etc. before
    making it the default.
    
    Fixes https://github.com/mixmaxhq/custody/issues/7.

--- bin/custody-cli.js
+++ bin/custody-cli.js
@@ -6,12 +6,19 @@ const argv = require('yargs')
   .usage('Usage: $0 [options]')
   .options({
     port: {
+      alias: 'p',
       describe: 'The port on which supervisor is running. 9001 is the default for Mixmax engineers.',
       type: 'number',
       default: 9001
+    },
+    notifications: {
+      alias: 'n',
+      describe: '`true` to enable desktop notifications when processes change state. Experimental--please contribute UX feedback!',
+      type: 'boolean',
+      default: false
     }
   })
-  .example('$0')
+  .example('$0 --notifications')
   .help('h')
   .alias('h', 'help')
   .argv;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
+import cloneDeep from 'lodash.clonedeep';
 import Console from './components/Console';
 import React from 'react';
 import {render} from 'react-blessed';
 import screen from './screen';
 import ProcessMonitor from './utils/processMonitor';
 
-export default async function start({ port }) {
+export default async function start({ port, notifications }) {
   const processMonitor = new ProcessMonitor({ supervisor: { port } }).on('error', (err) => {
     // TODO(jeff): Distinguish between fatal and non-fatal errors.
     console.error(err);
@@ -12,7 +13,12 @@ export default async function start({ port }) {
   });
 
   function renderApp() {
-    render(<Console processes={processMonitor.processes}/>, screen);
+    render(<Console
+      // HACK(jeff): Deep-clone the processes so that the console can diff changes. Although I'm not
+      // totally sure if this is hacky, like whose responsibility it should be for deep-cloning.
+      processes={cloneDeep(processMonitor.processes)}
+      notifications={notifications}
+    />, screen);
   }
 
   // Load all processes, then render, to avoid a flash as they load in (including probe states).
