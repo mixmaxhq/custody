7165c6ed490c2abd6ca198c18bff9b7629827a07
Jeff Wear <jeffreywear@gmail.com>
2018-05-16 15:31:49 -0700

    Add documentation to process utilities

--- src/utils/process.js
+++ src/utils/process.js
@@ -1,8 +1,27 @@
+/**
+ * States of a process' lifecycle.
+ */
 export const STATES = {
+  // The process is running.
   RUNNING: 'RUNNING',
+
+  // The process has exited with an error and will not be restarted.
   FATAL: 'FATAL'
 };
 
+/**
+ * Determines the "effective" state and description of a process.
+ *
+ * Normally this returns the process' own values for this information. However, if the process
+ * is running, but a subprocess is not running, then this will return the information for the
+ * subprocess since then the top-level process is not actually being effective at doing work.
+ *
+ * @param {Object} process - The process to analyze.
+ *
+ * @return {Object}
+ *  @param {String} state
+ *  @param {String} description
+ */
 export function effectiveState(process) {
   const immediateState = process.statename;
   const immediateDescription = process.description;
@@ -14,5 +33,5 @@ export function effectiveState(process) {
       return { state: childState, description: childDescription };
     }
   }
-  return { state: immediateState, description: immediateDescription};
+  return { state: immediateState, description: immediateDescription };
 }
